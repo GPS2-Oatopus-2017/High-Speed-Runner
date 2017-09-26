616a9d5d17152db28f5ad0aa4c19265fd555a465
FattyMieo <fong2009@live.com.my>
2017-09-27 02:58:28 +0800

    Finished Core Controls Schemes

---
+++ Assets/PlayerCoreController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+interface IPlayerComponent
+{
+	void SetPlayer(PlayerCoreController m_Player);
+}
+
+[RequireComponent (typeof(RigidbodyFirstPersonController))]
+[RequireComponent (typeof(PlayerAnimationController))]
+public class PlayerCoreController : MonoBehaviour
+{
+	public RigidbodyFirstPersonController rigidController;
+	public PlayerAnimationController animController;
+
+	void Awake()
+	{
+		rigidController = GetComponent<RigidbodyFirstPersonController>();
+		animController = GetComponent<PlayerAnimationController>();
+
+		if(rigidController) rigidController.SetPlayer(this);
+		if(animController) animController.SetPlayer(this);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	//Cross-object functions
+	//...
+}
