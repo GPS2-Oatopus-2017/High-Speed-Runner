93070fb90552f88e2ad303d93bc7598d572be923
FattyMieo <fong2009@live.com.my>
2017-10-06 23:40:08 +0800

    Editor Finished

--- Assets/Editor/WaypointNodeEditor.cs
+++ Assets/Editor/WaypointNodeEditor.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 public class WaypointNodeEditor : Editor
 {
 	SerializedProperty data;
+	bool backNodeFoldout = true;
 
 	// Use this for initialization
 	public override void OnInspectorGUI()
@@ -14,6 +15,33 @@ public class WaypointNodeEditor : Editor
 		serializedObject.Update();
 
 		data = serializedObject.FindProperty ("data");
+
+		if(data.FindPropertyRelative("backNodes").arraySize > 0)
+		{
+			GUI.enabled = false;
+			if(data.FindPropertyRelative("backNodes").arraySize == 1)
+			{
+				EditorGUILayout.PropertyField(data.FindPropertyRelative("backNodes").GetArrayElementAtIndex(0), new GUIContent("Back Node"));
+			}
+			else
+			{
+				backNodeFoldout = EditorGUILayout.Foldout(backNodeFoldout, "Back Nodes");
+				if(backNodeFoldout)
+				{
+					EditorGUI.indentLevel++;
+					for(int i = 0; i < data.FindPropertyRelative("backNodes").arraySize; i++)
+					{
+						EditorGUILayout.PropertyField(data.FindPropertyRelative("backNodes").GetArrayElementAtIndex(i), GUIContent.none);
+					}
+					EditorGUI.indentLevel--;
+				}
+			}
+			GUI.enabled = true;
+		}
+		else
+		{
+			EditorGUILayout.HelpBox("This node has no previous node.", MessageType.Warning);
+		}
 
 		EditorGUILayout.PropertyField(data.FindPropertyRelative("isJunction"));
 
@@ -26,6 +54,8 @@ public class WaypointNodeEditor : Editor
 		{
 			EditorGUILayout.PropertyField(data.FindPropertyRelative("node1"), new GUIContent("Forward Node"));
 		}
+
+		EditorGUILayout.HelpBox("To reset this node, click on \"Reset Node\" instead of \"Reset\".", MessageType.Info);
 
 		serializedObject.ApplyModifiedProperties();
 	}
--- Assets/WaypointNodeScript.cs
+++ Assets/WaypointNodeScript.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class NodeData
+public class WaypointNodeData
 {
 	public bool isJunction;
 
 	public WaypointNodeScript node1;
 	public WaypointNodeScript node2;
+
+	public List<WaypointNodeScript> backNodes;
 
 	public WaypointNodeScript forwardNode
 	{
@@ -57,38 +59,145 @@ public class NodeData
 				node2 = value;
 		}
 	}
+
+	public WaypointNodeData()
+	{
+		backNodes = new List<WaypointNodeScript>();
+	}
 }
 
 [System.Serializable]
+[ExecuteInEditMode]
 public class WaypointNodeScript : MonoBehaviour
 {
-	public NodeData data;
+	public WaypointNodeData data;
+	public WaypointNodeData prevData;
 
-	// Use this for initialization
-	void Start ()
+	void OnValidate()
 	{
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
+		UpdateJunctionMode();
+		UpdateBackNodes();
+		BackupPreviousData();
 	}
 
+	void OnDestroy()
+	{
+		ResetNodes();
+	}
+
+	[ContextMenu("Reset Nodes")]
+	void ResetNodes()
+	{
+		if(data.node1)
+		{
+			if(data.node1.data.backNodes.Contains(this))
+				data.node1.data.backNodes.Remove(this);
+
+			data.node1 = null;
+		}
+		if(data.node2)
+		{
+			if(data.node2.data.backNodes.Contains(this))
+				data.node2.data.backNodes.Remove(this);
+
+			data.node2 = null;
+		}
+		BackupPreviousData();
+	}
+
+	void UpdateJunctionMode()
+	{
+		if(data.isJunction != prevData.isJunction)
+		{
+			if(data.node1)
+			{
+				if(data.node1.data.backNodes.Contains(this))
+					data.node1.data.backNodes.Remove(this);
+				
+				data.node1 = null;
+			}
+			if(data.node2)
+			{
+				if(data.node2.data.backNodes.Contains(this))
+					data.node2.data.backNodes.Remove(this);
+				
+				data.node2 = null;
+			}
+
+			prevData.isJunction = data.isJunction;
+		}
+	}
+
+	void UpdateBackNodes()
+	{
+		if(!data.isJunction)
+		{
+			if(data.forwardNode)
+			{
+				if(!data.forwardNode.data.backNodes.Contains(this))
+					data.forwardNode.data.backNodes.Add(this);
+			}
+			else if(prevData.forwardNode)
+			{
+				if(prevData.forwardNode.data.backNodes.Contains(this))
+					prevData.forwardNode.data.backNodes.Remove(this);
+			}
+		}
+		else
+		{
+			if(data.leftNode)
+			{
+				if(!data.leftNode.data.backNodes.Contains(this))
+					data.leftNode.data.backNodes.Add(this);
+			}
+			else if(prevData.leftNode)
+			{
+				if(prevData.leftNode.data.backNodes.Contains(this))
+					prevData.leftNode.data.backNodes.Remove(this);
+			}
+
+			if(data.rightNode)
+			{
+				if(!data.rightNode.data.backNodes.Contains(this))
+					data.rightNode.data.backNodes.Add(this);
+			}
+			else if(prevData.rightNode)
+			{
+				if(prevData.rightNode.data.backNodes.Contains(this))
+					prevData.rightNode.data.backNodes.Remove(this);
+			}
+		}
+	}
+
+	void BackupPreviousData()
+	{
+		prevData.forwardNode = data.forwardNode;
+		prevData.leftNode = data.leftNode;
+		prevData.rightNode = data.rightNode;
+	}
+
+	// Only In Play Mode
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.GetComponent<PlayerCoreController>())
+		if(Application.isPlaying)
 		{
-			Debug.Log("enter");
-			WayPointManagerScript.Instance.RegisterNode(this);;
+			if(other.GetComponent<PlayerCoreController>())
+			{
+				Debug.Log("enter");
+				WayPointManagerScript.Instance.RegisterNode(this);;
+			}
 		}
 	}
 
 	void OnTriggerExit(Collider other)
 	{
-		if(other.GetComponent<PlayerCoreController>())
+		if(Application.isPlaying)
 		{
-			Debug.Log("exit");
-			WayPointManagerScript.Instance.UnregisterNode(this);
+			if(other.GetComponent<PlayerCoreController>())
+			{
+				Debug.Log("exit");
+				WayPointManagerScript.Instance.UnregisterNode(this);
+			}
 		}
 	}
 }
