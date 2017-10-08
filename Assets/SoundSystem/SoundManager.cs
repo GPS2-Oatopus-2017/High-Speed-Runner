7168b356ac133ba97198b35f439b5d7f9650ce1e
Syabil <0114962@kdu-online.com>
2017-10-08 23:26:02 +0800

    Sound system 2.0
    Placed some things that might be put to use soon.

--- Assets/SoundSystem/SoundManager.cs
+++ Assets/SoundSystem/SoundManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+	//Pyramind's
+//	public GameObject audiosourcePrefab;
+
+	//Brackeys
 	public Sound[] sounds;
 
 	public static SoundManager instance;
@@ -21,6 +25,7 @@ public class SoundManager : MonoBehaviour {
 
 		DontDestroyOnLoad(gameObject);
 
+		//Brackeys
 		foreach (Sound audio in sounds)
 		{
 			audio.source = gameObject.AddComponent<AudioSource>();
@@ -29,9 +34,11 @@ public class SoundManager : MonoBehaviour {
 			audio.source.volume = audio.volume;
 			audio.source.pitch = audio.pitch;
 			audio.source.loop = audio.loop;
+			audio.source.spatialBlend = audio.spatialBlend;
 		}
 	}
-	
+		
+	//Brackeys
 	public void Play(string name)
 	{
 		Sound audio = Array.Find(sounds, sound => sound.name == name);
@@ -41,5 +48,31 @@ public class SoundManager : MonoBehaviour {
 			return;
 		}
 		audio.source.Play(); //AudioManager.instance.Play("*insert audioclip's name*");
+	}
+
+	//Pyramind's
+//	public void PlayAudio(AudioClip clip, GameObject objecttoplayOn, float volume, float pitch, bool loop = false, float spatialBlend = 1f)
+//	{
+//		//AudioSource.PlayClipAtPoint(clip, objecttoplayOn.transform.position); //AudioManager.instance.playatGo("clip name", gameObject);
+//		AudioSource myaudioSource = Instantiate(audiosourcePrefab).GetComponent<AudioSource>();
+//		myaudioSource.Play(); //AudioManager.instance.PlayAudio("clip name", gameObject, 0f, 0f, 0f);
+//		myaudioSource.name = clip.name + objecttoplayOn.name;
+//		myaudioSource.gameObject.transform.position = objecttoplayOn.transform.position;
+//		myaudioSource.gameObject.transform.parent = objecttoplayOn.gameObject.transform;
+//		myaudioSource.clip = clip;
+//		myaudioSource.volume = volume;
+//		myaudioSource.pitch = pitch; //Add Random.Range(0.8f, 1.2f)
+//		myaudioSource.loop = loop;
+//		myaudioSource.spatialBlend = spatialBlend;
+//
+//		Destroy(myaudioSource.gameObject, myaudioSource.clip.length);
+//	}
+
+	public void StopAudio(string soundtostopName)
+	{
+		GameObject soundtostopObject = GameObject.Find(soundtostopName);
+		soundtostopObject.GetComponent<AudioSource>().Stop();
+
+		Destroy(soundtostopObject); //AudioManager.instance.StopAudio("object name".name + gameObject.name);
 	}
 }
