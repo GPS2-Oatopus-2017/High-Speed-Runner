ef5cf8013c84f456a19bcffa95f238cc702460d5
Syabil <0114962@kdu-online.com>
2017-10-10 18:11:34 +0800

    Scripted and added revamped Sound System. Edited MainMenuManager script for audio purposes. Added an sfx and few bgms.

--- Assets/SoundSystem/BGMLoader.cs
+++ Assets/SoundSystem/BGMLoader.cs
@@ -3,31 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BGMLoader : MonoBehaviour {
-
+public class BGMLoader : MonoBehaviour
+{
+	public AudioClipID bgm;
 	void Start()
 	{
-		if(SceneManager.GetActiveScene().name == "MainMenu")
-		{
-			SoundManager.instance.Play("Main Menu BGM");
-		}
-
-		if(SceneManager.GetActiveScene().name == "WaypointScene")
-		{
-			SoundManager.instance.Play("Test BGM");
-		}
-	}
-
-	void Update()
-	{
-		if(SceneManager.GetActiveScene().name != "MainMenu")
-		{
-			SoundManager.instance.Stop("Main Menu BGM");
-		}
-
-		if(SceneManager.GetActiveScene().name != "WaypointScene")
-		{
-			SoundManager.instance.Stop("Test BGM");
-		}
+		SoundManagerScript.Instance.PlayBGM(bgm);
 	}
 }
