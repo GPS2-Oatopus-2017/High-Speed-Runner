03a1e4630baddeae5038f2780bb0e3bd1eeb965a
xldeathgazelx <0115190@kdu-online.com>
2017-10-19 20:47:20 +0800

    Win / Lose Scene Transition:
    - Reaching the "end-point" will result in the transition into [WinScene]
    - Inability to reach the "end-point" within the given time will result in the transition into [LoseScene]
    - Once player's health reaches 0, scene will transition into [LoseScene]

---
+++ Assets/WinLoseConditions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseConditions : MonoBehaviour 
+{
+    public float timer;
+    public float setTimer = 10f;
+
+    public TimerScript timerScript;
+
+	void Start () 
+    {
+        timer = setTimer; // Count-down timer's value is set to desired amount of time at the beginning of the level
+	}
+
+	void Update () 
+    {
+        CharacterDeath();
+
+        if(timerScript.hasStarted == true) // After the [hasStarted] boolean from the TimerScript is set to true, start [TimesUp] function.
+        {
+            TimesUp();
+        }
+	}
+
+    void TimesUp()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0) // After count-down timer reaches "0" change scene to [LoseScene]
+        {
+            GetComponent<ChangeSceneScript>().ChangeScenes(1); 
+        }
+    }
+
+    void CharacterDeath()
+    {
+        if(PlayerScript.Instance.health <= 0) //After character health reaches "0" change scene to [LoseScene]
+        {
+            GetComponent<ChangeSceneScript>().ChangeScenes(1);
+        }
+    }
+}
